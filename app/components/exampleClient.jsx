df4c53e3e715015dc0e2ad771ec9183d58f314c9
Marian Primak <knochen2@gmail.com>
2023-12-01 11:39:17 -0800

    set up Auth

---
+++ app/components/exampleClient.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ServerC from "./exampleServer";
+
+export default function exampleClient() {
+  console.log("Iam a Client Component");
+
+  return (
+    <div>
+      <div>Example is a CLIENT</div>
+    </div>
+  );
+}
---
+++ app/components/exampleServer.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+export default function exampleServer() {
+  console.log("Iam a Server Component");
+  return <div>This example comes from the Server</div>;
+}
